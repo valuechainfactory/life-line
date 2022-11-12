7f4a25ade754a6ad93d05c3f59a2f9c43f1339d3
kwambiee <kwambokaj2.jk@gmail.com>
2022-11-12 17:43:44 +0300

    Fix linters

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,40 +1,39 @@
 class PatientsController < ApplicationController
+  def index
+    @patients = Patient.all
+  end
 
-    def index
-        @patients = Patient.all
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    respond_to do |format|
+      if @patient.save
+        format.html { redirect_to patients_path, notice: 'Patient was successfully created.' }
+      else
+        flash[:alert] = 'Patient was not created.'
+        render :new, locals: { patient: @patient }
+      end
     end
+  end
 
-    def show
-        @patient = Patient.find(params[:id])
+  def update
+    @patient = Patient.find(params[:id])
+    respond_to do |format|
+      if @patient.update(patient_params)
+        format.html { redirect_to patients_path, notice: 'Patient was successfully updated.' }
+      else
+        flash[:alert] = 'Patient was not updated.'
+        render :edit, locals: { patient: @patient }
+      end
     end
+  end
 
-    def create
-        @patient = Patient.new(patient_params)
-        respond_to do |format|
-            if @patient.save
-                format.html { redirect_to patients_path, notice: 'Patient was successfully created.'}
-            else
-                flash[:alert] = "Patient was not created."
-                render :new, locals: { patient: @patient }
-            end
-        end
-    end
+  private
 
-    def update
-        @patient = Patient.find(params[:id])
-        respond_to do |format|
-            if @patient.update(patient_params)
-                format.html { redirect_to patients_path, notice: 'Patient was successfully updated.' }
-            else
-                flash[:alert] = "Patient was not updated."
-                render :edit, locals: { patient: @patient }
-            end
-        end
-    end
-
-    private
-
-    def patient_params
-        params.require(:patient).permit(:first_name, :last_name, :national_id, :image, :telephone, :gender, :doctor_id)
-    end
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :national_id, :image, :telephone, :gender, :doctor_id)
+  end
 end
--- spec/requests/patients_spec.rb
+++ spec/requests/patients_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Patients', type: :request do
   before :each do
     @patient = Patient.create(first_name: 'John', last_name: 'Doe', national_id: '123456789', image: 'image.jpg',
-                              telephone: '0712345678', gender: 'male')
+                              telephone: '0712345678', gender: 'male', doctor_id: 1)
   end
 
   describe 'GET /index' do
@@ -18,7 +18,7 @@ RSpec.describe 'Patients', type: :request do
 
     it 'returns http success' do
       Patient.create(first_name: 'John', last_name: 'Doe', national_id: '123456789',
-                     image: 'image.jpg', telephone: '0712345678', gender: 'male')
+                     image: 'image.jpg', telephone: '0712345678', gender: 'male', doctor_id: 2)
       get "/patients/#{patient.id}"
       expect(response).to have_http_status(:success)
     end
@@ -29,7 +29,7 @@ RSpec.describe 'Patients', type: :request do
       expect do
         post '/patients',
              params: { patient: { first_name: 'Jane', last_name: 'Doe', national_id: '123456789', image: 'image.jpg',
-                                  telephone: '0712345678', gender: 'Female' } }
+                                  telephone: '0712345678', gender: 'Female', doctor_id: 1 } }
       end.to change(Patient, :count).by(1)
     end
   end
@@ -40,7 +40,7 @@ RSpec.describe 'Patients', type: :request do
     it 'updates a patient' do
       patch "/patients/#{patient.id}",
             params: { patient: { first_name: 'Mercy', last_name: 'Wakili', national_id: '123456789', image: 'image.jpg',
-                                 telephone: '0712345678', gender: 'Female' } }
+                                 telephone: '0712345678', gender: 'Female', doctor_id: 1 } }
       expect(response).to redirect_to('/patients')
     end
   end
