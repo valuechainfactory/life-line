ed1f66b2705c96dababe3447d9a1c9fc1c36b08c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-12 12:49:37 +0300

    Added create and destroy methods for preexisting conditions

--- app/controllers/pre_existing_conditions_controller.rb
+++ app/controllers/pre_existing_conditions_controller.rb
@@ -1,2 +1,24 @@
 class PreExistingConditionsController < ApplicationController
+
+    def create
+        @pre_existing_condition = PreExistingCondition.new(pre_existing_condition_params)
+        if @pre_existing_condition.save
+            redirect_to @pre_existing_condition
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @pre_existing_condition = PreExistingCondition.find(params[:id])
+        @pre_existing_condition.destroy
+        redirect_to pre_existing_conditions_path
+    end
+
+
+    private
+    def pre_existing_condition_params
+        params.require(:pre_existing_condition).permit(:diabetes, :hypertension, :asthma)
+    end
+
 end
