46d5ad97586e33da3dc56bea707c5e08b4a72935
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-12 12:22:29 +0300

    orted schema errors

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_084556) do
     t.string "first_name"
     t.string "second_name"
     t.string "phone_number"
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_081438) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "drug_allergies", force: :cascade do |t|
     t.string "name"
