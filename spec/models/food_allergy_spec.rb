7d2a435adf0e4d25d5dee0e9c51e27e702106f44
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-12 12:04:38 +0300

    Added test files

--- spec/models/food_allergy_spec.rb
+++ spec/models/food_allergy_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 RSpec.describe FoodAllergy, type: :model do
-
-    it "should not be valid without a name" do
-        food_allergy = FoodAllergy.new(name: nil)
-        expect(food_allergy).to_not be_valid
-    end
-  
-  it "should be valid with a name" do
-    foodallergy = FoodAllergy.create(name: "Peanuts")
-    expect(foodallergy).to be_valid
+  it 'should not be valid without a name' do
+    food_allergy = FoodAllergy.new(name: nil)
+    expect(food_allergy).to_not be_valid
   end
 
-
+  it 'should be valid with a name' do
+    foodallergy = FoodAllergy.create(name: 'Peanuts')
+    expect(foodallergy).to be_valid
+  end
 end
