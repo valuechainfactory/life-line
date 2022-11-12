9187e798bc20e33a5bbd5c083713856441d19395
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-12 14:38:01 +0300

    Added relationships

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,6 @@ class Doctor < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :patients
 end
--- app/models/food_allergy.rb
+++ app/models/food_allergy.rb
@@ -1,3 +1,5 @@
 class FoodAllergy < ApplicationRecord
   validates :name, presence: true
+  belongs_to :patient
+  belongs_to :pre_existing_condition, through: :conditions_foods
 end
