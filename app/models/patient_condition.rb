2388174353bc2c8e8795a779f71cce2367ee4ceb
kwambiee <kwambokaj2.jk@gmail.com>
2022-11-12 14:19:51 +0300

    Add association to patient and pre-existing condition model

--- app/models/patient_condition.rb
+++ app/models/patient_condition.rb
@@ -1,2 +1,7 @@
 class PatientCondition < ApplicationRecord
+    validates :patient_id, presence: true, numericality: { only_integer: true }
+    validates :pre_existing_condition_id, presence: true, numericality: { only_integer: true }
+
+    belongs_to :patient, optional: true
+    belongs_to :pre_existing_condition, optional: true
 end
--- app/models/pre_existing_condition.rb
+++ app/models/pre_existing_condition.rb
@@ -1,2 +1,5 @@
 class PreExistingCondition < ApplicationRecord
+
+    has_many :patient_conditions, dependent: :destroy
+    has_many :patients, through: :patient_conditions, dependent: :destroy
 end
