6427907fc0b920b94c867e5ef0217574e3714f22
David Abol Ger <draker74@gmail.com>
2022-11-12 14:09:39 +0300

    add users remove linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :doctor_id , :hospital_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name doctor_id hospital_name])
   end
 end
